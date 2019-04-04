91919e206c2c870ee595c235ad989fe275158823
Rakesh Yadav <44271099+RakeshY008@users.noreply.github.com>
2019-04-04 22:43:40 +0530

    Update scratch_2.py

--- Getting started With Python 3._/scratch_2.py
+++ Getting started With Python 3._/scratch_2.py
@@ -1,5 +1,7 @@
-# Aim : Use of Math fun.
-# Author : Rakesh Yadav
+'''
+Aim : Use of Math fun.
+Author : Rakesh Yadav
+'''
 
 value = 12.32
 value1 = 21
@@ -9,7 +11,3 @@ print(math.ceil(value1))
 print(math.ceil(value))
 print(math.floor(value1))
 print(math.floor(value))
-
-
-
-
