136dd3fe00df5d57d176546a497fb90f7c3c3fd0
Rakesh Yadav <44271099+RakeshY008@users.noreply.github.com>
2019-04-15 19:54:14 +0530

    Update vow_name.py
    Documentation modification...

--- 2nd Sem.-python pdf/vow_name.py
+++ 2nd Sem.-python pdf/vow_name.py
@@ -1,4 +1,12 @@
-m=input()
-if ('a' or 'A' and 'i' or 'I' and 'u' or 'U' and 'o' or 'O' and 'e' or 'E') in m :
-	print(m)
+'''
+Author : Rakesh Yadav
+Task: To kame a program in which 
+	1. take an input as string from user 
+	2. check the string if it contains all vowels or not
+	3. print if it satisfy above conditions.
+'''
+
+STR = input()
+if ('a' or 'A' and 'i' or 'I' and 'u' or 'U' and 'o' or 'O' and 'e' or 'E') in STR :
+	print(STR)
 
