8df9e99673632e27e839990d05593177b705e13d
Rakesh Yadav <44271099+RakeshY008@users.noreply.github.com>
2019-04-15 20:06:59 +0530

    Update check_hetrogen.py
    Modifications in source code as well as documentations...

--- 2nd Sem.-python pdf/check_hetrogen.py
+++ 2nd Sem.-python pdf/check_hetrogen.py
@@ -1,20 +1,23 @@
-S=input()
-S=S.lower()
-S[0]="h"
+'''
+Author: Rakesh yadav
+Tasks : write a programe in pyhton which
+	1. take an input from user as string
+	2. check if it is a " herrogram " string or not
+	3. print ' Hetrogram '  if it is , otherwise print ' Not Hertogram '
+'''
 
+STR = input()
+STR = STR.lower() 			#to convert string into lower-case.
 n=0
-for n in range(len(S)):	
-	
-	m=S.count(S[n])
+for n in range(len(STR)):		
+	m = STR.count(S[n])		#method to check no. of times that value in input string
 	if m<2:
 		c=1
 	elif m>1:
 		c=2
-		break		
-
-	
+		break			
 if c==2:
-	print("not hertogram")
+	print("Not Hertogram")
 else:
-	print("hetrogram")
+	print("Hetrogram")
 
