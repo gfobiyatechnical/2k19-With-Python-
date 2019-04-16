6e2f2f0e0caf3e802d7b7bdb28bb27e8656ff3f8
Rakesh Yadav <44271099+RakeshY008@users.noreply.github.com>
2019-04-16 22:42:27 +0530

    Update fun_with_turtle.py
    some modifications in source-code, i will soon complete it..........

--- 2nd Sem.-python pdf/fun_with_turtle.py
+++ 2nd Sem.-python pdf/fun_with_turtle.py
@@ -1,27 +1,77 @@
-import turtle
+# Author : Rakesh Yadav
 
-turtle.bgcolor("white")
+
+import turtle
+turtle.bgcolor("black")
 turtle.pensize(4)
+turtle.speed(100)
+
 
 def Yadav_ji():
-    for i in range(300):
+    turtle.speed(100)
+    for i in range(200):
+        turtle.speed(100)
         turtle.right(1)
         turtle.forward(1)
-        
-turtle.speed(15)
-turtle.color("yellow","pink")
 
+def blank():
+    turtle.speed(100)
+    turtle.color("black")
+    turtle.right(40)
+    turtle.forward(120)
+    I()
+    
+def I():
+    turtle.speed(100)
+    turtle.color("white")
+    turtle.left(0)
+    turtle.forward(50)
+    turtle.color("pink")
+    turtle.backward(25)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(25)
+    turtle.color("pink")
+    turtle.backward(50)
+    
+    turtle.color("black")
+    turtle.left(-145)
+    turtle.forward(70)
+    turtle.right(120)
+    lv()
+
+def lv():
+    turtle.speed(100)
+    turtle.color("pink")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(45)
+    
+    turtle.left(120)
+    turtle.forward(35)
+    turtle.backward(35)
+    turtle.right(55)
+    turtle.forward(35)
+    turtle.backward(35)
+
+    turtle.color("black")
+    turtle.left(-145)
+    turtle.forward(150)
+
+
+        
+turtle.speed(100)
+turtle.color("red","red")
 turtle.begin_fill()
 turtle.left(140)
-turtle.forward(111.65)
+turtle.forward(150.65)
 Yadav_ji()
-
-
-turtle.left(120)
+turtle.left(140)
 Yadav_ji()
-
-turtle.forward(111.48)
+turtle.forward(149.50)
 turtle.end_fill()
-turtle.hideturtle()
-#Yadav_ji()
 
+blank()
+
+#turtle.hideturtle()
